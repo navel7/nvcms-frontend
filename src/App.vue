fd27e35e274b606d0e87625fdd63164b99e03b10
navel7 <4985857+navel7@users.noreply.github.com>
2020-01-24 22:42:08 +0900

    Fix code formatting

--- src/App.vue
+++ src/App.vue
@@ -23,11 +23,7 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
         <span class="mr-2">Latest Release</span>
         <v-icon>fas fa-external-link-alt</v-icon>
       </v-btn>
